4e164f7dd18b4b5dc361a61b3aeff136a39cb6ce
bmtestoc <60735933+bmtestoc@users.noreply.github.com>
2021-01-03 19:59:17 +0100

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+//const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const userRoutes = require('./routes/user');
+/*const postRoutes = require('./routes/post');
+const commentRoutes = require('./routes/comment');*/
+const app = express();
+const helmet = require('helmet');
+
+// Securisation entêtes HTTP
+app.use(helmet());
+
+// Active CORS pour éviter les attaques CSRF
+/*app.use(cors({
+  origin: 'http://localhost:4200'
+}));*/
+
+
+/*
+// Connexion à MongoDB
+mongoose.connect('mongodb+srv://login:mdp@sopekocko.hx6se.mongodb.net/p6ocr?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+*/
+
+// Pour pouvoir utiliser les données du corps de la requête
+app.use(bodyParser.json());
+
+// Pour la gestion des images
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Routes API
+app.use('/api/user', userRoutes);
+/*
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+*/
+
+module.exports = app;
